a0fe9149c33b9b0ec823f1d126e3de0770006d31
Bassam Aziz <bassam.aziz@itsmycargo.com>
2020-11-16 21:13:53 +0100

    single product endpoint

--- app/graphql/query_types/product_query_type.rb
+++ app/graphql/query_types/product_query_type.rb
@@ -6,5 +6,11 @@ module QueryTypes
     field :products, types[Types::Product], 'returns all products' do
       resolve ->(_obj, _args, _ctx) { Product.all }
     end
+
+    field :product, Types::Product, 'returns the queried product' do
+      argument :id, !types.ID
+
+      resolve ->(_obj, args, _ctx) { Product.find_by!(id: args[:id]) }
+    end
   end
 end
--- spec/graphql/query_types/product_query_type_spec.rb
+++ spec/graphql/query_types/product_query_type_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe QueryTypes::ProductQueryType do
   types = GraphQL::Define::TypeDefiner.instance
   let!(:products) { create_list(:product, 3) }
-
+  let(:product) { products.first }
   describe 'querying all products' do
     it 'has a :products that returns a product type' do
       expect(subject).to have_field(:products).that_returns(types[Types::Product])
@@ -15,6 +15,15 @@ RSpec.describe QueryTypes::ProductQueryType do
       end
 
       expect(query_result.count).to eq(products.count)
+    end
+  end
+
+  describe 'querying a specific product using it\'s id' do
+    it 'returns the queried product' do
+      args = { id: product.id }
+      query_result = subject.fields['product'].resolve(nil, args, nil)
+
+      expect(query_result).to eq(product)
     end
   end
 end
